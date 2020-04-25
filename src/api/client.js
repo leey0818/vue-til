83a0d19ea3ca447d2ea72b015c1ac1e16b141809
leey0818 <leey0818@gmail.com>
2020-04-25 16:14:26 +0900

    api 인증만료로 인한 로그아웃 처리 추가

--- src/api/client.js
+++ src/api/client.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 import store from '@/store';
+import router from '@/routers';
+import bus from '@/utils/bus';
+import { LOGOUT } from '@/store/mutation-types';
 
 const authInterceptor = config => {
   config.headers['Authorization'] = store.state.token;
@@ -44,6 +47,18 @@ class ApiAuthClient extends ApiClient {
   constructor(baseUrl) {
     super(baseUrl);
     this.client.interceptors.request.use(authInterceptor);
+    this.client.interceptors.response.use(undefined, error => {
+      const { response } = error;
+
+      // 인증정보 만료
+      if (response && response.status === 401) {
+        store.commit(LOGOUT);
+        bus.$emit('show:toast', '인증 정보가 만료되어 재 로그인이 필요합니다.');
+        router.push('/login');
+      }
+
+      return Promise.reject(error);
+    });
   }
 }
 
