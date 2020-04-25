c403a71cd5a10d3ef319027545b99b057dec45bb
leey0818 <leey0818@gmail.com>
2020-04-25 16:10:50 +0900

    내용에 따라 toast 너비 자동조정

--- src/components/common/ToastPopup.vue
+++ src/components/common/ToastPopup.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="toast" :class="toastAnimationClass">
-    <span>{{ message }}</span>
+    <div class="inner-box">
+      <span>{{ message }}</span>
+    </div>
   </div>
 </template>
 
@@ -46,20 +48,24 @@ export default {
 <style scoped>
 .toast {
   position: fixed;
-  left: 50%;
   bottom: -120px;
-  width: 340px;
-  padding: 15px;
-  margin-left: -170px;
-  background-color: #36343e;
-  border-radius: 2px;
-  box-shadow: 0 3px 5px 0 rgba(0, 0, 0, 0.4);
-  color: white;
-  text-align: center;
+  width: 100%;
+  display: flex;
+  justify-content: center;
   transition: transform 750ms ease-out;
 }
 .toast.show {
   transform: translateY(-140px);
   transition: transform 500ms ease-in-out;
+}
+.inner-box {
+  min-width: 340px;
+  max-width: 500px;
+  padding: 15px;
+  background-color: #36343e;
+  border-radius: 2px;
+  box-shadow: 0 3px 5px 0 rgba(0, 0, 0, 0.4);
+  color: white;
+  text-align: center;
 }
 </style>
